285984dae5ae1ab7c72a129f6cc4c133fe3e5955
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-06-03 19:37:43 +0200

    Completed animation in hero section
    Added animation in hero section

--- src/home/featured/Text.js
+++ src/home/featured/Text.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { easyPolyOut } from "d3-ease";
 import Animate from "react-move/Animate";
 
+//import FeaturedPlayer from '../../Resources/images/featured_player.png'
+
 class Text extends Component {
   animateNumber = () => (
     <Animate
@@ -32,8 +34,107 @@ class Text extends Component {
       }}
     </Animate>
   );
+
+  animateFirst = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 450
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [450],
+        timing: { duration: 300, delay: 500 },
+        transitionTimingFunction: { easyPolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_first"
+            style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`
+            }}
+          >
+            League
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  animateSecond = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 586
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [586],
+        timing: { duration: 500, delay: 500 },
+        transitionTimingFunction: { easyPolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_second"
+            style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`
+            }}
+          >
+            Championships
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  animatePlayer = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0
+      }}
+      enter={{
+        opacity: [1],
+        timing: { duration: 500, delay: 1000 },
+        transitionTimingFunction: { easyPolyOut }
+      }}
+    >
+      {({ opacity }) => {
+        return (
+          <div
+            className="featured_second"
+            style={{
+              opacity,
+              background: `url(${FeaturedPlayer})`,
+              transform: `translate(550px,201px)`
+            }}
+          />
+        );
+      }}
+    </Animate>
+  );
+
   render() {
-    return <div className="featured_text">{this.animateNumber()}</div>;
+    return (
+      <div className="featured_text">
+        {this.animatePlayer()}
+        {this.animateNumber()}
+        {this.animateFirst()}
+        {this.animateSecond()}
+      </div>
+    );
   }
 }
 
