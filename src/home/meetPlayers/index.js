f698da46b75e33cd918001c5a5a3c60c96b35265
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-06-17 18:56:45 +0200

    Player Card
    Added player card section with a small animation

--- src/home/meetPlayers/index.js
+++ src/home/meetPlayers/index.js
@@ -1,79 +1,95 @@
 import React, { Component } from "react";
 import Stripes from "../../Resources/images/stripes.png";
 import { Tag } from "../../ui/misc";
+import Reveal from "react-reveal/Reveal";
+import HomeCard from "./cards";
 
 class MeetPlayers extends Component {
-  state = {};
+  state = {
+    show: false
+  };
 
   render() {
     return (
-      <div
-        className="home_meetplayers"
-        style={{ background: `#fff url(${Stripes})` }}
+      <Reveal
+        fraction={0.7}
+        onReveal={() => {
+          this.setState({
+            show: true
+          });
+          console.log("reveal");
+        }}
       >
-        <div className="container">
-          <div className="home_meetplayers_wrapper">
-            <div className="home_card_wrapper">Card</div>
-            <div className="home_text_wrapper">
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#fff"
-                  add={{
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  Meet
-                </Tag>
+        <div
+          className="home_meetplayers"
+          style={{ background: `#fff url(${Stripes})` }}
+        >
+          <div className="container">
+            <div className="home_meetplayers_wrapper">
+              <div className="home_card_wrapper">
+                <HomeCard show={this.state.show} />
               </div>
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#fff"
-                  add={{
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  The
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#fff"
-                  add={{
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  Players
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  bck="#fff"
-                  size="27px"
-                  color="#0e1731"
-                  link={true}
-                  linkto="/the_team"
-                  add={{
-                    display: "inline-block",
-                    marginBottom: "27px",
-                    border: "1px solid #0e1731"
-                  }}
-                >
-                  Meet the here
-                </Tag>
+              <div className="home_text_wrapper">
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#fff"
+                    add={{
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    Meet
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#fff"
+                    add={{
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    The
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#fff"
+                    add={{
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    Players
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#fff"
+                    size="27px"
+                    color="#0e1731"
+                    link={true}
+                    linkto="/the_team"
+                    add={{
+                      display: "inline-block",
+                      marginBottom: "27px",
+                      border: "1px solid #0e1731"
+                    }}
+                  >
+                    Meet the here
+                  </Tag>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Reveal>
     );
   }
 }
