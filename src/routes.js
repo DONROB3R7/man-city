37c56f3c81ea97d2b91c15dde3c2808fa4d1c15d
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-06-28 20:17:31 +0200

    Public  Route
    Added Public Route logic :
    When the user are log in there are not able to see sign_in component even though it's a public route.But there are abele to see home component with it's a public route even so if you are log in or log out.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "./HOC/Layout";
 import { Switch, Route } from "react-router-dom";
+
 import PrivateRoute from "./Componets/authRoutes/privateRountes";
+import PublicRoute from "./Componets/authRoutes/publicRoutes";
 
 import Home from "../src/home";
 import SignIn from "../src/signin";
@@ -17,8 +19,20 @@ const Routes = props => {
           exact
           component={Dashboard}
         />
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PublicRoute
+          {...props}
+          path="/sign_in"
+          restricted={true}
+          exact
+          component={SignIn}
+        />
+        <PublicRoute
+          {...props}
+          path="/"
+          restricted={false}
+          exact
+          component={Home}
+        />
       </Switch>
     </Layout>
   );
