177966360433d4ebe7bcec0535ee69bbdc35be04
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-07-01 18:16:32 +0200

    Admin Matches Section
    Added the table from  material-ui core fetch  the information from fire base to feillup the table.Added the logic for result class  also added a preload progress to fill the time until firebase bring all the information into a Json.

--- src/admin/matches/index.js
+++ src/admin/matches/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
+import { firebaseMatches } from "../../firebase";
+import { firebaseLooper, reverseArray } from "../../ui/misc";
 import AdminLayout from "../../HOC/AdminLayout";
 
 import Table from "@material-ui/core/Table";
@@ -8,10 +11,79 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class AdminMatcheds extends Component {
+  state = {
+    isLoading: true,
+    matches: []
+  };
+
+  componentDidMount() {
+    firebaseMatches.once("value").then(snapshot => {
+      const matches = firebaseLooper(snapshot);
+
+      this.setState({
+        isLoading: false,
+        matches: reverseArray(matches)
+      });
+    });
+  }
+
   render() {
-    return <AdminLayout>table</AdminLayout>;
+    return (
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Match</TableCell>
+                  <TableCell>Result</TableCell>
+                  <TableCell>Final</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.matches
+                  ? this.state.matches.map((match, i) => (
+                      <TableRow key={i}>
+                        <TableCell>{match.date}</TableCell>
+                        <TableCell>
+                          <Link to={`/admin_matches/edit_match/${match.id}`}>
+                            {match.away} <strong>-</strong> {match.local}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          {match.resultAway} <strong>-</strong>{" "}
+                          {match.resultLocal}
+                        </TableCell>
+
+                        <TableCell>
+                          {match.final === "Yes" ? (
+                            <span className="matches_tag_red">Finale</span>
+                          ) : (
+                            <span className="matches_tag_green">
+                              Not Played yet
+                            </span>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  : null}
+              </TableBody>
+            </Table>
+          </Paper>
+          <div className="admin_progress">
+            {this.state.isLoading ? (
+              <CircularProgress thickness={7} style={{ color: "#98c5e9" }} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </AdminLayout>
+    );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./HOC/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 import PrivateRoute from "./Componets/authRoutes/privateRountes";
 import PublicRoute from "./Componets/authRoutes/publicRoutes";
